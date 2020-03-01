c1168af91592218790b938f1f9adc4462430257d
Lucas Owino <lucowish35@gmail.com>
2020-03-01 21:25:54 +0300

    Add function to take order with java script

--- js/script.js
+++ js/script.js
@@ -26,3 +26,45 @@ $(document).ready(function(){
         $(".cr").show();
     });
 });
+
+function getSizeValue(){
+    var choosenValue = document.getElementsByClassName("S").value;
+    return parseInt(choosenValue); 
+}
+function getToppings(){
+    var choosenToppings = document.getElementsByClassName("T").value;
+    return parseInt(choosenToppings); 
+}
+function getCrusts(){
+    var choosenCrusts = document.getElementsByClassName("C").value;
+    return parseInt(choosenCrusts); 
+}
+function getQuantity(){
+    var choosenQuantity = document.getElementsByClassName("No").value;
+    return parseInt(choosenQuantity); 
+}
+function addTotal(){
+    var choosenNumber = (getSizeValue() + getToppings() + getCrusts()) * getQuantity();
+    alert("You have Ordered " + getQuantity("")  +  " pizza."  +  ""  +  " The Total Amount is kshs "  +  (choosenNumber)  +  ""  +  " Thank you for your order welcome again.");
+}
+
+function getSizeValue(){
+    var choosenValue = document.getElementsByClassName("Ss").value;
+    return parseInt(choosenValue); 
+}
+function getToppings(){
+    var choosenToppings = document.getElementsByClassName("T").value;
+    return parseInt(choosenToppings); 
+}
+function getCrusts(){
+    var choosenCrusts = document.getElementsByClassName("C").value;
+    return parseInt(choosenCrusts); 
+}
+function getQuantity(){
+    var choosenQuantity = document.getElementsByClassName("No").value;
+    return parseInt(choosenQuantity); 
+}
+function addTotalToo(){
+    var choosenNumber = (getSizeValue() + getToppings() + getCrusts()) * getQuantity();
+    alert("You have Ordered " + getQuantity("")  +  " pizza."  +  ""  +  " The Total Amount is kshs "  +  (choosenNumber)  +  ""  +  " Thank you for your order welcome again.");
+}
